35e975b5f1f86f712c39a317d5413e1fd6f8b811
youhai.cai <1548176306@qq.com>
2018-01-11 16:24:18 +0800

    tinker热修复的gradle配置

---
+++ app/src/main/java/com/cyh/tinker/sample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.cyh.tinker.sample;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //加载补丁包
+        String path= Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk";
+        TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), path);
+
+        text=findViewById(R.id.text);
+
+        text.setText(getStrText());
+
+    }
+
+    private String getStrText(){
+//        return "这个APP有bug";
+        return "这个APP的bug已修复";
+    }
+}
